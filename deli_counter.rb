6c32094ee9d7f6a8f154dcb68ffa7c723641132d
aeisha simmons <ashicks2017@outlook.com>
2019-05-16 11:43:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,18 +13,17 @@ take_a_number(katz_deli,"Alan")
 
 
 def line(katz_deli)
-
-def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty"
   else
     message = "The line is currently:"
   katz_deli.each_with_index do |value, index|
     message += "#{index.to_i+1}. #{value}"
+  end
+   puts "#{message}"
+  end
 end
-  puts "#{message}"
-end
-end
+
 line(katz_deli)
 
 def now_serving(katz_deli)
