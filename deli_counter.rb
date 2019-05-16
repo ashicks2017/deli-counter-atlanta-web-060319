ec744ea9a852f71da9b62a3d2c0e57ac7a54c0ca
aeisha simmons <ashicks2017@outlook.com>
2019-05-16 10:55:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,12 +8,13 @@ def take_a_number(katz_deli, name)
 end
 
 def take_a_numbers(katz_deli,name)
-  katz_deli.each do |name|
   place_in_line = katz_deli.index(name)
+  katz_deli.each do |name|
   puts "Welcome, #{name}. You are number #{place_in_line+1} in line."
   return name, place_in_line
  end
 end
+
 take_a_numbers(katz_deli,"Grace")
 take_a_numbers(katz_deli,"Tom")
 take_a_numbers(katz_deli,"Alan")
