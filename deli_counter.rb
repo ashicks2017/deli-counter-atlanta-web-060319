6009a348a878f84dd2e1338020c3afffbbf40eba
aeisha simmons <ashicks2017@outlook.com>
2019-05-16 10:33:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+# Write your code
+katz_deli = []
+def take_a_number(array, name)
+  array << name
+  place_in_line = array.index(name)
+  puts "Welcome, #{name}. You are #{array.index(name)+1} in line"
+  
+  return name, place_in_line
+end
+take_a_number(katz_deli,"Grace")
+take_a_number(katz_deli,"Tom")
+take_a_number(katz_deli,"Alan")
